c1b7fdd6586d13bd375d329c0807309df2f65614
Ketha Charan <charan.ketha1747@gmail.com>
2021-05-11 22:19:01 -0700

    [Charan] Add: Player Starting Position

---
+++ SnakeAndLadder/SnakeAndLadder/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace SnakeAndLadder
+{
+    class SnakeAndLadder
+    {
+        int START_POSITION = 0;
+        public void PlayGame()
+        {
+            Random random = new Random();
+            int die = random.Next(1, 7);
+            Console.WriteLine("Player Position : " + die);
+            Console.WriteLine("Single Player Start Position:  " + START_POSITION);
+            Console.ReadLine();
+        }
+        static void Main(string[] args)
+        {
+            SnakeAndLadder StartPlay = new SnakeAndLadder();
+            StartPlay.PlayGame();
+        }
+    }
+}
